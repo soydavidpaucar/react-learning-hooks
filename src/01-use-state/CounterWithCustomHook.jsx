39ca0ab99dd0faa59d304e26f64fb5e38dc06afd
David Paucar <soydavidpaucar@gmail.com>
2022-11-03 18:11:11 -0500

    :art: imported and used custom hook which handle counter logic

--- src/01-use-state/CounterWithCustomHook.jsx
+++ src/01-use-state/CounterWithCustomHook.jsx
@@ -1,11 +1,22 @@
+import { useCounter } from '../hooks/useCounter';
+
 export const CounterWithCustomHook = () => {
+  const { count, increment, reset, decrement } = useCounter();
+
   return (
     <>
-      <h1>Counter With Custom Hook: </h1>
+      <h1>Counter With Custom Hook: {count}</h1>
       <hr />
 
-      <button className='btn btn-primary'>+1</button>
-      <button className='btn btn-warning'>-1</button>
+      <button className='btn btn-primary' onClick={() => increment(5)}>
+        +1
+      </button>
+      <button className='btn btn-secondary' onClick={reset}>
+        Reset
+      </button>
+      <button className='btn btn-warning' onClick={decrement}>
+        -1
+      </button>
     </>
   );
 };
