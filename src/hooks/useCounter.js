47cee25b48a4bdbacc5d969b46b4f53dd9f8ddf7
David Paucar <soydavidpaucar@gmail.com>
2022-11-03 18:10:39 -0500

    :art: created custom hook which store counter logic

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export const useCounter = (initialValue = 10) => {
+  const [count, setCount] = useState(initialValue);
+  const increment = (number = 1) => setCount(count + number);
+  const reset = () => setCount(initialValue);
+  const decrement = () =>
+    setCount(() => {
+      if (count > 0) {
+        return count - 1;
+      }
+      return count;
+    });
+  return { count, increment, reset, decrement };
+};
