10bdfa6ba85f4eebfeade54135c3559edaf17200
David Paucar <soydavidpaucar@gmail.com>
2022-11-03 17:31:05 -0500

    :art: created number in object to see how to preserve values

--- src/01-use-state/CounterApp.jsx
+++ src/01-use-state/CounterApp.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 
 export const CounterApp = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30
+  });
+
+  const { counter1, counter2, counter3 } = state;
 
   return (
     <>
-      <h1>Counter: {counter}</h1>
+      <h1>Counter: {counter1}</h1>
+      <h1>Counter: {counter2}</h1>
+      <h1>Counter: {counter3}</h1>
       <hr />
-      <button className='btn btn-primary' onClick={() => setCounter(counter + 1)}>
+      <button className='btn btn-primary' onClick={() => setCounter({ ...state, counter1: counter1 + 1 })}>
         +1
       </button>
       <button className='btn btn-warning'>-1</button>
