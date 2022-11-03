b8cabc29cc44c131818faf87dd9e20cc55a0367b
David Paucar <soydavidpaucar@gmail.com>
2022-11-03 17:35:15 -0500

    :art: created component layout to work with

---
+++ src/01-use-state/CounterWithCustomHook.jsx
@@ -0,0 +1,11 @@
+export const CounterWithCustomHook = () => {
+  return (
+    <>
+      <h1>Counter With Custom Hook: </h1>
+      <hr />
+
+      <button className='btn btn-primary'>+1</button>
+      <button className='btn btn-warning'>-1</button>
+    </>
+  );
+};
