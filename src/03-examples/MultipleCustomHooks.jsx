03507e3a6fb54e1a9723894e4daf3c40aa514bcb
David Paucar <soydavidpaucar@gmail.com>
2022-11-05 17:19:05 -0500

    :art: created component to render quotes and uses two custom hooks

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,31 @@
+import { useCounter } from '../hooks/useCounter';
+import { useFetch } from '../hooks/useFetch';
+
+export const MultipleCustomHooks = () => {
+  const { count, increment } = useCounter(1);
+  const { data, isLoading, error } = useFetch(`https://www.breakingbadapi.com/api/quotes/${count}`);
+
+  /* Destructuring the data object. */
+  const { author, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <>
+          <blockquote className="blockquote text-end">
+            <p className="mb-3">{quote}</p>
+            <footer className="blockquote-footer">{author}</footer>
+          </blockquote>
+          <button className="btn btn-primary" onClick={() => increment()}>
+            Next quote
+          </button>
+        </>
+      )}
+    </>
+  );
+};
