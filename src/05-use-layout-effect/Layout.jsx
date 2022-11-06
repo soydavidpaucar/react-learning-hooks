23e9687b667848bf1db7888799bb49067e93fa31
David Paucar <soydavidpaucar@gmail.com>
2022-11-05 21:30:28 -0500

    :art: created component to see how useLayoutEffect works

---
+++ src/05-use-layout-effect/Layout.jsx
@@ -0,0 +1,23 @@
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from '../03-examples';
+
+export const Layout = () => {
+  const { count, increment } = useCounter(1);
+  const { data, isLoading, error } = useFetch(`https://www.breakingbadapi.com/api/quotes/${count}`);
+
+  /* Destructuring the data object. */
+  const { author, quote } = !!data && data[0];
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? <LoadingQuote /> : <Quote quote={quote} author={author} />}
+
+      <button className="btn btn-primary" onClick={() => increment()} disabled={isLoading}>
+        Next quote
+      </button>
+    </>
+  );
+};
