43d85f9e21749494763c0d99c863969efbdc97f7
David Paucar <soydavidpaucar@gmail.com>
2022-11-05 21:11:38 -0500

    :art: added code to understand how useRef works

--- src/04-use-ref/FocusScreen.jsx
+++ src/04-use-ref/FocusScreen.jsx
@@ -1,12 +1,31 @@
+import { useRef } from 'react';
+
 export const FocusScreen = () => {
+  /* Creating a reference to the input element. */
+  const inputRef = useRef();
+
+  /**
+   * The handleClick function is called when the button is clicked.
+   *
+   * The function uses the current property of the inputRef object to call the select() method on the
+   * input element.
+   *
+   * The select() method selects the contents of the input element
+   */
+  const handleClick = () => {
+    inputRef.current.select();
+  };
+
   return (
     <>
       <h1>Focus Screen</h1>
       <hr />
 
-      <input type="text" placeholder="Name..." className="form-control" />
+      <input type="text" placeholder="Name..." className="form-control" ref={inputRef} />
 
-      <button className="btn btn-primary mt-2">Set Focus</button>
+      <button className="btn btn-primary mt-2" onClick={handleClick}>
+        Set Focus
+      </button>
     </>
   );
 };
