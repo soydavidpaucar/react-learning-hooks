301edc6d7f8a9c77f30316964cf1de469dbe282e
David Paucar <soydavidpaucar@gmail.com>
2022-11-05 21:04:41 -0500

    :art: created component to see how useRef works

---
+++ src/04-use-ref/FocusScreen.jsx
@@ -0,0 +1,12 @@
+export const FocusScreen = () => {
+  return (
+    <>
+      <h1>Focus Screen</h1>
+      <hr />
+
+      <input type="text" placeholder="Name..." className="form-control" />
+
+      <button className="btn btn-primary mt-2">Set Focus</button>
+    </>
+  );
+};
