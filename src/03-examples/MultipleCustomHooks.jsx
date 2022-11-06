f95d1742e9131307854d64c2c997b495b012c2f4
David Paucar <soydavidpaucar@gmail.com>
2022-11-05 20:40:26 -0500

    :technologist: improved readability of components and imports

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,4 +1,5 @@
-import { useFetch, useCounter } from '../hooks';
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from './';
 
 export const MultipleCustomHooks = () => {
   const { count, increment } = useCounter(1);
@@ -12,19 +13,11 @@ export const MultipleCustomHooks = () => {
       <h1>BreakingBad Quotes</h1>
       <hr />
 
-      {isLoading ? (
-        <div className="alert alert-info text-center">Loading...</div>
-      ) : (
-        <>
-          <blockquote className="blockquote text-end">
-            <p className="mb-3">{quote}</p>
-            <footer className="blockquote-footer">{author}</footer>
-          </blockquote>
-          <button className="btn btn-primary" onClick={() => increment()}>
-            Next quote
-          </button>
-        </>
-      )}
+      {isLoading ? <LoadingQuote /> : <Quote quote={quote} author={author} />}
+
+      <button className="btn btn-primary" onClick={() => increment()} disabled={isLoading}>
+        Next quote
+      </button>
     </>
   );
 };
