9de1d13d315cedb8400ca129890d73c0029c41a9
David Paucar <soydavidpaucar@gmail.com>
2022-11-06 12:32:50 -0500

    :art: created code to explain how useMemo works

--- src/06-memos/MemoHook.jsx
+++ src/06-memos/MemoHook.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { useCounter } from '../hooks';
 
+const heavyStuff = (iterationNumber = 100) => {
+  for (let i = 0; i < iterationNumber; i++) {
+    console.log('Here');
+  }
+
+  return `${iterationNumber} realized iterations`;
+};
+
 export const MemoHook = () => {
-  const { count, increment } = useCounter(1);
+  const { count, increment } = useCounter(4000);
   const [show, setShow] = useState(true);
+
+  /* A hook that allows us to memorize a value, so it is not recalculated every time the component is
+  rendered. */
+  const memorizedValue = useMemo(() => heavyStuff(count), [count]);
 
   return (
     <>
       <h1>
         Counter: <small>{count}</small>
-        <hr />
       </h1>
+      <hr />
+
+      <h4>{memorizedValue}</h4>
 
       <button className="btn btn-primary" onClick={() => increment(1)}>
         +1
