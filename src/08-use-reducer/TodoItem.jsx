f3b4dfe9e70f0094d269bee7ad519241a6f63886
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 23:09:30 -0500

    :art: created action when todo is completed with it style

--- src/08-use-reducer/TodoItem.jsx
+++ src/08-use-reducer/TodoItem.jsx
@@ -1,7 +1,7 @@
-export const TodoItem = ({ todo, deleteTodo }) => {
+export const TodoItem = ({ todo, deleteTodo, toggleTodo }) => {
   return (
-    <li className="list-group-item d-flex justify-content-between">
-      <span className="align-self-center">{todo.description}</span>
+    <li className="list-group-item d-flex justify-content-between" onClick={() => toggleTodo(todo.id)}>
+      <span className={`align-self-center${todo.done ? ' text-decoration-line-through' : ''}`}>{todo.description}</span>
       <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>
         Delete
       </button>
