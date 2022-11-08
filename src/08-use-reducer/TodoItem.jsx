7b524ecf1130ec53afb0934ed3f75fa1962d1f94
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 22:50:00 -0500

    :art: added button action to delete todo by id

--- src/08-use-reducer/TodoItem.jsx
+++ src/08-use-reducer/TodoItem.jsx
@@ -1,8 +1,10 @@
-export const TodoItem = ({ todo }) => {
+export const TodoItem = ({ todo, deleteTodo }) => {
   return (
     <li className="list-group-item d-flex justify-content-between">
       <span className="align-self-center">{todo.description}</span>
-      <button className="btn btn-danger">Delete</button>
+      <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>
+        Delete
+      </button>
     </li>
   );
 };
