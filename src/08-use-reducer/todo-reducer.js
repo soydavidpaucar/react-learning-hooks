d074d5eca88fb73ad867eb682228423664bc03e6
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 23:07:26 -0500

    :art: created action to toggle todo if completed

--- src/08-use-reducer/todo-reducer.js
+++ src/08-use-reducer/todo-reducer.js
@@ -4,6 +4,16 @@ export const todoReducer = (initialState = [], action) => {
       return [...initialState, action.payload];
     case 'delete':
       return initialState.filter((todo) => todo.id !== action.payload);
+    case 'toggle':
+      return initialState.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            done: !todo.done
+          };
+        }
+        return todo;
+      });
     default:
       return initialState;
   }
