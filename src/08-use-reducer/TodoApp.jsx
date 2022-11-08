58bed0132eb117840effc4bc54e73542b2942713
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 21:22:34 -0500

    :art: created structure for the todo app

--- src/08-use-reducer/TodoApp.jsx
+++ src/08-use-reducer/TodoApp.jsx
@@ -18,20 +18,34 @@ const initialState = [
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
 
-  console.log('todos :>> ', todos);
-  console.log('dispatch :>> ', dispatch);
-
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>
+        TodoApp (10), <small>Pending: 2</small>
+      </h1>
       <hr />
 
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Add Todo</h4>
+          <hr />
+          <form>
+            <input type="text" placeholder="Add todo..." className="form-control" />
+
+            <button type="submit" className="btn btn-outline-primary mt-2 btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
