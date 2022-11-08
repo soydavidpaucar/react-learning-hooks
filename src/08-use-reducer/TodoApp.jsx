89172a9bc44791549285c73c26a78f8a4b3970a9
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 22:28:45 -0500

    :sparkles: added new todo and persistance data in local storage

--- src/08-use-reducer/TodoApp.jsx
+++ src/08-use-reducer/TodoApp.jsx
@@ -1,33 +1,46 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 
 import { todoReducer } from './todo-reducer';
 import { TodoAdd } from './TodoAdd';
 import { TodoList } from './TodoList';
 
 const initialState = [
-  {
+  /* {
     id: new Date().getTime(),
     description: 'Learn React',
     done: false
-  },
-  {
-    id: new Date().getTime() + 1,
-    description: 'Learn Node',
-    done: false
-  }
+  } */
 ];
 
+/**
+ * If there are todos in localStorage, parse them and return them, otherwise return an empty array
+ * @returns An array of todos.
+ */
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  /* Saving the todos in localStorage. */
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const handleNewTodo = (newTodo) => {
-    console.log(newTodo);
+    const action = {
+      type: 'add',
+      payload: newTodo
+    };
+
+    dispatch(action);
   };
 
   return (
     <>
       <h1>
-        TodoApp (10), <small>Pending: 2</small>
+        TodoApp {todos.length}, <small>Pending: 2</small>
       </h1>
       <hr />
 
