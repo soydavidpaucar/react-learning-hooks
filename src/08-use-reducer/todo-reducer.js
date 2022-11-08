94cf2764b1174cc4cf9741825238d5a244a11a31
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 22:48:26 -0500

    :sparkles: created action to delete a todo

--- src/08-use-reducer/todo-reducer.js
+++ src/08-use-reducer/todo-reducer.js
@@ -2,7 +2,8 @@ export const todoReducer = (initialState = [], action) => {
   switch (action.type) {
     case 'add':
       return [...initialState, action.payload];
-
+    case 'delete':
+      return initialState.filter((todo) => todo.id !== action.payload);
     default:
       return initialState;
   }
