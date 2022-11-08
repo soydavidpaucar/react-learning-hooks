327472157ffd98bcbf997db6acd100975804f057
David Paucar <soydavidpaucar@gmail.com>
2022-11-07 22:49:40 -0500

    :art: send delete function to it child component

--- src/08-use-reducer/TodoList.jsx
+++ src/08-use-reducer/TodoList.jsx
@@ -1,10 +1,10 @@
 import { TodoItem } from './TodoItem';
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos, handleDeleteTodo }) => {
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} deleteTodo={handleDeleteTodo} />
       ))}
     </ul>
   );
