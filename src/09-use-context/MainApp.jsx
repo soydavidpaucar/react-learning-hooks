0eaa36d283969772483a471b9e0ca5c506a112f5
David Paucar <soydavidpaucar@gmail.com>
2022-11-08 22:22:31 -0500

    :sparkles: added routes to the respective component

--- src/09-use-context/MainApp.jsx
+++ src/09-use-context/MainApp.jsx
@@ -1,8 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+
 export const MainApp = () => {
   return (
     <div>
       <h1>MainApp</h1>
       <hr />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 };
