3c3b4c95886fcc9c2f6ae0554f74b0a60d73060b
David Paucar <soydavidpaucar@gmail.com>
2022-11-08 22:31:09 -0500

    :art: imported Navbar component

--- src/09-use-context/MainApp.jsx
+++ src/09-use-context/MainApp.jsx
@@ -1,13 +1,15 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
 
 import { AboutPage } from './AboutPage';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
   return (
     <div>
       <h1>MainApp</h1>
+      <Navbar />
       <hr />
 
       <Routes>
