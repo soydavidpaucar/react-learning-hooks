fe70bbd1c38094d6c8dcfbf834bfe1633dce8b89
David Paucar <soydavidpaucar@gmail.com>
2022-11-08 22:46:28 -0500

    :art: added menu with its items and styles depends on the route

--- src/09-use-context/Navbar.jsx
+++ src/09-use-context/Navbar.jsx
@@ -1,11 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="login">Login</Link>
-      <Link to="about">About</Link>
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark rounded-3">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          useContext
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className={({ isActive }) => `nav-link${isActive ? ' active' : ''}`}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="about" className={({ isActive }) => `nav-link${isActive ? ' active' : ''}`}>
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="login" className={({ isActive }) => `nav-link${isActive ? ' active' : ''}`}>
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
