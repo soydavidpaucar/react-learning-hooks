7456fca5d651bb101533c6afb3c1b83e1261cdc8
David Paucar <soydavidpaucar@gmail.com>
2022-11-09 22:53:13 -0500

    :art: imported context to use the data stored globally

--- src/09-use-context/HomePage.jsx
+++ src/09-use-context/HomePage.jsx
@@ -1,8 +1,16 @@
+import { useContext } from 'react';
+
+import { UserContext } from './context/UserContext';
+
 export const HomePage = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <h1>HomePage</h1>
       <hr />
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
 };
--- src/09-use-context/LoginPage.jsx
+++ src/09-use-context/LoginPage.jsx
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
+
+import { UserContext } from './context/UserContext';
+
 export const LoginPage = () => {
+  /* Destructuring the `user` property from the `UserContext` object. */
+  const { user, setUser } = useContext(UserContext);
+
   return (
     <div>
       <h1>LoginPage</h1>
       <hr />
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <button className="btn btn-primary" onClick={() => setUser({ id: 1, name: 'David', email: 'react@react.com' })}>
+        Set user
+      </button>
     </div>
   );
 };
