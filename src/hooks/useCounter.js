61b40ffab198916e4e5c9972363da01a34067207
David Paucar <soydavidpaucar@gmail.com>
2022-11-13 15:10:35 -0500

    :art: updated counter and decrement to use the current value

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -3,14 +3,14 @@ import { useState } from 'react';
 export const useCounter = (initialValue = 10) => {
   const [count, setCount] = useState(initialValue);
 
-  const increment = (number = 1) => setCount(count + number);
+  const increment = (number = 1) => setCount((current) => current + number);
   const reset = () => setCount(initialValue);
   const decrement = () =>
-    setCount(() => {
-      if (count > 0) {
-        return count - 1;
+    setCount((current) => {
+      if (current > 0) {
+        return current - 1;
       }
-      return count;
+      return current;
     });
   return { count, increment, reset, decrement };
 };
